60012ec2607bb71fdef13b1018f84e074dfc882f
Bruno Bastos <bruno.ufrgs2019@gmail.com>
2025-02-27 19:40:49 -0300

    Main finished

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from src.compressor import Compressor
 from skimage.io import imread
+from matplotlib import pyplot as plt
 
+compressor = Compressor(imread('input_images/AerialCity_3840x1920_30fps_8bit_420_erp_0.bmp', as_gray=True).astype(float))			
 
-compressor = Compressor(imread('input_images/AerialCity_3840x1920_30fps_8bit_420_erp_0.bmp'))
-compressor.our_methodology()
-print(compressor.get_image())
+print(f"The image will be compressed by a quality factor of {compressor.get_quantization_factor()}."); print()
+# Change the quality factor
+# compressor.set_quantization_factor('Set here')
+
+print(f"Transformation matrix:")
+print(compressor.get_transformation_matrix()); print()
+# compressor.set_transformation_matrix('Set here') # TB, TO, TR
+
+print(f"Quantization matrix:")
+print(compressor.get_quantization_matrix()); print()
+# compressor.set_quantization_matrix('Set here') # Q0, QB, QHVS
+
+original_image = compressor.get_image()
+compressor.lower_complexity(); print()
+reconstructed_image = compressor.get_image()
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+axes[0].imshow(original_image, cmap='gray')
+axes[0].set_title('Original Image')
+axes[0].axis('off')
+
+axes[1].imshow(reconstructed_image, cmap='gray')
+axes[1].set_title('Compressed Image')
+axes[1].axis('off')
+
+fig.tight_layout()
+plt.show()
+
--- src/compressor.py
+++ src/compressor.py
@@ -1,22 +1,27 @@
 from src.matrices import *
+from scipy import signal
 import numpy as np
 
 class Compressor:
+		
+	wspnr = None
+	wssim = None
+	bpp = None
+	
 	def __init__(self, image:np.ndarray=None, block_size:int=8, transformation_matrix:np.ndarray=TR, quantization_matrix:np.ndarray=Q0, quantization_factor:int=50, np2:str='Round'):
 		""" Compress image """
+		self.image = image
 		if image is None:
 			raise ValueError("Image is required")
-		self.image = image
+		if image.max() <= 1:
+			self.image = np.around(255*image)
 		self.block_size = block_size
+		self.quantization_factor = quantization_factor
 		self.transformation_matrix = transformation_matrix
 		self.quantization_matrix = quantization_matrix
-		self.dequantization_matrix = None
-		self.quantization_factor = quantization_factor
+		self.dequantization_matrix = quantization_matrix			
 		self.np2 = np2
-		self.wpsnr = None
-		self.wssim = None
-		self.bpp = None
-
+		
 	# Getters and Setters
 	def get_image(self):
 		return self.image
@@ -69,56 +74,36 @@ class Compressor:
 	def set_np2(self, np2:str):
 		self.np2 = np2
 
+	@staticmethod
+	def umount(image_data:np.ndarray, block_size:int=8):
+		""" Unmounts a matrix into blocks  """
+		nrows, ncols = block_size, block_size
+		h, w= image_data.shape
+		if h % nrows != 0 or w % ncols != 0:
+			raise ValueError(f"The matrix ({h}x{w}) cannot be divided exactly into blocks of {nrows}x{ncols}")
+		return image_data.reshape(h//nrows, nrows, -1, ncols).swapaxes(1,2).reshape(-1, nrows, ncols)
 
+	@staticmethod
+	def remount(image_data:np.ndarray, new_dimension:tuple[int, int]):
+		""" Remounts a matrix into blocks  """
+		h, w = new_dimension
+		_, nrows, ncols = image_data.shape
+		return image_data.reshape(h//nrows, -1, nrows, ncols).swapaxes(1,2).reshape(h, w)
 
-	def umount(data:np.ndarray, newdimension:tuple=(8, 8)):
-		if len(newdimension) == 1:
-			n = newdimension[0]
-			return [data[i:i + n] for i in range(0, len(data), n)]
-		elif len(newdimension) == 2:
-			nrows, ncols = newdimension
-			h, w = data.shape
-			if h % nrows != 0 or w % ncols != 0:
-				raise ValueError(f"The matrix ({h}x{w}) cannot be divided exactly into blocks of {nrows}x{ncols}")
-			return (data.reshape(h//nrows, nrows, -1, ncols).swapaxes(1,2).reshape(-1, nrows, ncols))
-
-	def remount(data:np.ndarray, newdimension:tuple=(8, 8)):
-		if len(newdimension) == 1:
-			return [value for chunk in data for value in chunk]
-		elif len(newdimension) == 2:
-			h, w = newdimension
-			_, nrows, ncols = data.shape
-			return (data.reshape(h//nrows, -1, nrows, ncols).swapaxes(1,2).reshape(h, w))
-		
-	def compute_adjustment_matrix(transformation_matrix:np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-		""" Compute adjustment matrix obtained by the transformation matrix """
-		adjustment_matrix = np.array(np.sqrt(np.linalg.inv(np.dot(transformation_matrix, transformation_matrix.T)))) 
-		adjustment_vector = np.array(np.diag(adjustment_matrix))
-		return adjustment_matrix, adjustment_vector
-	
-	def scale_quantization(quality_factor:int, quantization_matrix:np.ndarray) -> np.ndarray:
-		""" Adjust quantization matrix by a scalar factor """
-		s = 5000.0 / quality_factor if quality_factor < 50 else 200.0 - 2.0 * quality_factor
-		return np.floor((s * quantization_matrix + 50) / 100)
-	
-	def compute_quantization_dequantization_matrices(self, transformation_matrix:np.ndarray, quantization_matrix:np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-		""" Compute quantization and dequantization matrices """
-		S, s = self.compute_adjustment_matrix(transformation_matrix)
-		Z = np.dot(s.T, s)
-		Q_forward = np.divide(quantization_matrix, Z)
-		Q_backward =  np.multiply(quantization_matrix,  Z)
-		return Q_forward, Q_backward
-	
-	def map_k_and_el(row_index:int, image_height:int) -> tuple:
+	@staticmethod
+	def map_k_and_el(row_index:int, image_height:int) -> tuple[int, float]:
+		""" Calculates the elevation angle (el) of an image line and the indices (k) for distortions """
 		el = row_index/image_height * np.pi - np.pi/2
 		kprime = np.arange(8)
-		k = np.clip(0, 7, np.around(kprime/np.cos(el))).astype(int)
-		return (k, el)
-
-	def build_LUT(self, image_height:int, N:int=8) -> tuple:
+		k = np.clip(np.around(kprime/np.cos(el)), 0, 7).astype(int)
+		return k, el
+		
+	@staticmethod
+	def build_LUT(image_height:int, N:int=8) -> tuple:
+		""" Builds the Look-up-table of the image based on its height """
 		ks, els = [], []
 		for row_index in range(0, image_height+1, N):
-			k, el = self.map_k_and_el(row_index, image_height)
+			k, el = Compressor.map_k_and_el(row_index, image_height)
 			ks.append(k); els.append(el)
 		ks = np.asarray(ks); els = abs(np.asarray(els))
 		k_LUT = np.unique(ks, axis=0)
@@ -139,8 +124,10 @@ class Compressor:
 				max_LUT.append(aux_max_LUT[idx])
 		max_LUT = np.asarray(max_LUT)
 		return k_LUT, min_LUT, max_LUT
-
-	def QtildeAtEl(k_lut:np.ndarray, min_lut:np.ndarray, max_lut:np.ndarray, el:np.float32, quantization_matrix:np.ndarray, QF:int= 50):
+	
+	@staticmethod
+	def QtildeAtEl(quantization_matrix:np.ndarray, k_lut:np.ndarray, min_lut:np.ndarray, max_lut:np.ndarray, el:np.float32):
+		""" Returns the quantization matrix for a given elevation angle """
 		ks = None
 		Q = []
 		el = abs(el) # LUT is mirrored
@@ -154,49 +141,141 @@ class Compressor:
 		Q = Q.T
 		return Q
 	
-	def prepareQPhi(self, image:np.ndarray, quantization_matrix:np.ndarray, N = 8):
+	def __scale_quantization(self) -> np.ndarray:
+		""" Adjust quantization matrix by a scalar factor """
+		s = 5000.0 / self.quantization_factor if self.quantization_factor < 50 else 200.0 - 2.0 * self.quantization_factor
+		self.quantization_matrix = np.floor((s * self.quantization_matrix + 50) / 100)
+	
+	def __compute_quantization_dequantization_matrices(self) -> tuple[np.matrix, np.matrix]:
+		""" Compute quantization and dequantization matrices through the transformation matrix """
+		adjustment_matrix = np.matrix(np.sqrt(np.linalg.inv(np.dot(self.transformation_matrix, self.transformation_matrix.T)))) 
+		adjustment_vector = np.matrix(np.diag(adjustment_matrix))
+		Z = np.dot(adjustment_vector.T, adjustment_vector)
+		self.quantization_matrix = np.divide(self.quantization_matrix, Z)
+		self.dequantization_matrix =  np.multiply(self.dequantization_matrix,  Z)
+	
+	def __prepareQPhi(self, original_image_shape:tuple):
 		""" Builds the Look-up-table of the image based on its height """
-		h, w = image.shape
-		k_lut, min_lut, max_lut = self.build_LUT(h)
-		els = np.linspace(-np.pi/2, np.pi/2, h//N+1)
+		h, w = original_image_shape
+		k_lut, min_lut, max_lut = Compressor.build_LUT(h)
+		els = np.linspace(-np.pi/2, np.pi/2, h//self.block_size+1)
 		els = 0.5*(els[1:] + els[:-1]) # gets the "central" block elevation
-		QPhi = []
+		QPhiForward, QPhiBackward = [], []
 		for el in els: 
-			QPhi.append(self.QtildeAtEl(k_lut, min_lut, max_lut, el, quantization_matrix))
-		QPhi = np.asarray(QPhi)
-		QPhi = np.repeat(QPhi, w//N, axis=0)
-		return QPhi
+			QPhiForward.append(Compressor.QtildeAtEl(self.quantization_matrix, k_lut, min_lut, max_lut, el))
+			QPhiBackward.append(Compressor.QtildeAtEl(self.dequantization_matrix, k_lut, min_lut, max_lut, el))
+		QPhiForward = np.asarray(QPhiForward)
+		QPhiBackward = np.asarray(QPhiBackward)
+		self.quantization_matrix = np.repeat(QPhiForward, w//self.block_size, axis=0)
+		self.dequantization_matrix = np.repeat(QPhiBackward, w//self.block_size, axis=0)
 
-	def direct_transform(self, block_size:int):
+	def __direct_transform(self):
 		""" 2D DCT """
-		self.image = np.einsum('mij, jk -> mik', np.einsum('ij, mjk -> mik', self.transformation_matrix, self.umount(self.image, (block_size, block_size))), self.transformation_matrix.T)
+		self.image = np.einsum('mij, jk -> mik', np.einsum('ij, mjk -> mik', self.transformation_matrix, self.image), self.transformation_matrix.T)
 		
-	def quantize(self):
+	def __quantize(self):
 		""" Quantization forward """
 		self.image = np.around(np.divide(self.image, self.quantization_matrix))
 
-	def dequantize(self):
+	def __dequantize(self):
 		""" Quantization backward """
 		self.image = np.multiply(self.image, self.dequantization_matrix)
 
-	def inverse_transform(self, block_size:int):
+	def __inverse_transform(self):
 		""" 2D IDCT """
 		self.image = np.einsum('mij, jk -> mik', np.einsum('ij, mjk -> mik', self.transformation_matrix.T, self.image), self.transformation_matrix)
 
-	def np2_round(quantization_matrix:np.ndarray) -> np.ndarray:
-		return np.power(2, np.around(np.log2(quantization_matrix)))
-	""" Approximation function for powers of two based on the rounding function - Oliveira (2016) """
+	def __np2_round(self) -> np.ndarray:
+		""" Approximation function for powers of two based on the rounding function - Oliveira (2016) """
+		self.quantization_matrix = np.power(2, np.around(np.log2(self.quantization_matrix)))
+		self.dequantization_matrix = np.power(2, np.around(np.log2(self.dequantization_matrix)))
 
-	def np2_ceil(quantization_matrix:np.ndarray) -> np.ndarray:
-		return np.power(2, np.ceil(np.log2(quantization_matrix))) # Don't use this
-	""" Approximation function for powers of two based on the ceiling function - Brahimi (2021) """
+	def __np2_ceil(self) -> np.ndarray:# Don't use this
+		""" Approximation function for powers of two based on the ceiling function - Brahimi (2021) """
+		self.quantization_matrix = np.power(2, np.ceil(np.log2(self.quantization_matrix)))
+		self.dequantization_matrix = np.power(2, np.ceil(np.log2(self.dequantization_matrix)))
 
-	def np2_floor(quantization_matrix:np.ndarray) -> np.ndarray:
-		return np.power(2, np.floor(np.log2(quantization_matrix))) # Don't use this
-	""" Approximation function for powers of two based on the flooring function - Us (2025) """
+	def __np2_floor(self) -> np.ndarray: # Don't use this
+		""" Approximation function for powers of two based on the flooring function - Us (2025) """
+		self.quantization_matrix = np.power(2, np.floor(np.log2(self.quantization_matrix)))
+		self.dequantization_matrix = np.power(2, np.floor(np.log2(self.dequantization_matrix)))
 
-	@classmethod
-	def our_methodology(self):
-		""" Compress image """
-		print("Compressing image...")
+	def __calculate_bpp(self):
+		""" Calculate bits per pixel """
+		self.bpp = np.count_nonzero(np.logical_not(np.isclose(self.image, 0))) * 8 / (self.image.shape[0] * self.image.shape[1])
+
+	def __calculate_wpsnr(self, original_image:np.ndarray, max_value:int=255):
+		""" Calculate Weighted-to-Spherically-Uniform PSNR """
+		def __weights(height, width):
+			phis = np.arange(height+1) * np.pi / height
+			deltaTheta = 2 * np.pi / width 
+			column = np.asarray([deltaTheta * (-np.cos(phis[j+1])+np.cos(phis[j])) for j in range(height)])
+			return np.repeat(column[:, np.newaxis], width, 1)
+		w = __weights(original_image.shape[0], original_image.shape[1])
+		wmse = np.sum((original_image - self.image)**2 * w) / (4 * np.pi)
+		self.wpsnr = 10 * np.log10(max_value**2 / wmse)
+
+	def __calculate_wssim(self, original_image:np.ndarray, K1:float=0.01, K2:float=0.03, L:int=255):
+		""" Calculate Weighted-to-Spherically-Uniform SSIM """
+		def __fspecial_gauss(size:int, sigma:float):
+			""" Function to mimic fspecial('gaussian',...) from MATLAB """
+			x, y = np.mgrid[-size//2 + 1:size//2 + 1, -size//2 + 1:size//2 + 1]
+			g = np.exp(-((x**2 + y**2)/(2.0*sigma**2)))
+			return g / g.sum()
 		
+		def __weights(height, width):
+			deltaTheta = 2 * np.pi / width 
+			column = np.asarray([np.cos( deltaTheta * (j - height/2.+0.5)) for j in range(height)])
+			return np.repeat(column[:, np.newaxis], width, 1)
+		
+		k = 11
+		sigma = 1.5
+		window = __fspecial_gauss(k, sigma)
+		window2 = np.zeros_like(window); window2[k//2, k//2] = 1
+
+		C1 = (K1 * L)**2
+		C2 = (K2 * L)**2
+
+		mu1 = signal.convolve2d(original_image.astype(float), window, 'valid')
+		mu2 = signal.convolve2d(self.image.astype(float), window, 'valid')
+
+		mu1_sq = mu1 * mu1
+		mu2_sq = mu2 * mu2
+		mu1_mu2 = mu1 * mu2
+
+		sigma1_sq = signal.convolve2d(original_image.astype(float)*original_image.astype(float), window, 'valid') - mu1_sq
+		sigma2_sq = signal.convolve2d(self.image.astype(float)*self.image.astype(float), window, 'valid') - mu2_sq
+		sigma12 = signal.convolve2d(original_image.astype(float)*self.image.astype(float), window, 'valid') - mu1_mu2
+
+		W = __weights(*original_image.astype(float).shape)
+		Wi = signal.convolve2d(W, window2, 'valid')
+
+		ssim_map = ((2*mu1_mu2 + C1)*(2*sigma12 + C2))/((mu1_sq + mu2_sq + C1)*(sigma1_sq + sigma2_sq + C2)) * Wi
+		self.wsmssim = sum(ssim_map)/sum(Wi)
+
+	
+	def lower_complexity(self):
+		""" The base proposal of our algorithm.  """
+		original_image = self.image
+		
+		print("Compressing image...")
+		self.image = Compressor.umount(self.image, self.block_size)
+		self.__direct_transform()
+		self.__compute_quantization_dequantization_matrices()
+		self.__scale_quantization()
+		self.__np2_round()
+		self.__prepareQPhi(original_image.shape)
+		self.__quantize()
+
+		self.compressed_image = np.clip(Compressor.remount(self.image, (original_image.shape[0], original_image.shape[1])), 0, 255)
+		self.bpp = self.__calculate_bpp()
+
+		self.__dequantize()
+		self.__inverse_transform()
+		self.image = np.clip(Compressor.remount(self.image, (original_image.shape[0], original_image.shape[1])), 0, 255)
+
+		self.__calculate_wpsnr(original_image)
+		self.__calculate_wssim(original_image)
+
+		print("Image compressed successfully")
+
